c9b7a768703f0405897a0740db62914d93b5f1f5
takitec <lloydtacs123@gmail.com>
2024-07-20 10:59:15 +0800

    Update apiService.js

--- src/component/Service/apiService.js
+++ src/component/Service/apiService.js
@@ -2,18 +2,18 @@ import axios from 'axios';
 
 const API_URL = 'https://benzel-sb.yeems214.xyz';
 
-export const login = async (credentials) => {
+export const login = async (username, password) => {
   try {
-    const response = await axios.post(`${API_URL}/login`, credentials);
+    const response = await axios.post(`${API_URL}/login`, { username, password });
     return response.data;
   } catch (error) {
     throw error.response.data;
   }
 };
 
-export const signup = async (accDetails) => {
+export const signup = async (id, username, email, password, confirmPass) => {
   try {
-    const response = await axios.post(`${API_URL}/signup`, accDetails);
+    const response = await axios.post(`${API_URL}/signup`, { id, username, email, password, confirmPass });
     return response.data;
   } catch (error) {
     throw error.response.data;
